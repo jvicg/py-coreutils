255e7f09bc74810fdf2c29ea06d9dbd3893607d5
nrk19 <j@nrk.com>
2023-12-05 15:20:48 +0100

    adding working version of mvv3.py

--- mvv3.py
+++ mvv3.py
@@ -1,45 +1,50 @@
 #!/usr/bin/env python3
+# mvv3.py
+# simple python script to execute mv or rsync depends on the file size
+# it will show a progress bar when file size is greater than SIZE_FOR_PROGRESS_BAR (in MB)
 
 import os
 import sys
+import funcs
 import subprocess
 
-MIN_SIZE_FOR_RSYNC = 100
-
-def get_file_size(f):
-    return os.stat(f).st_size
+# vars
+SIZE_FOR_PROGRESS_BAR = 100    # minimum file size to show the progress bar
+flags = []                     # list with all the given files by the user
+sources = []                   # all the files or dirs that will be moved
+rsync_sources = []             # same but with progress bar
 
 # handle arguments
-flags = []
-sources = []
-rsync_sources = []
 
-for arg in sys.argv[1:]:
-
+for arg in sys.argv[1:-1]:
     # string with the pattern '-*' will be considered as a flag
     if arg.startswith("-"):
         flags.append(arg)
-
     # check is args are files indeed
     elif os.path.isfile(arg) or os.path.isdir(arg):
-        sources.append(arg)
+        # check file size
+        if funcs.get_size(arg) > SIZE_FOR_PROGRESS_BAR:
+            rsync_sources.append(arg)
+        else:
+            sources.append(arg)
     else:
         print(f'ERROR: {arg} is not a valid file or directory')
         sys.exit(1)
 
-if os.path.isdir(sources[-1]):
-    destination = sources.pop(len(sources)-1)
+# last arg must be a directory since is there where we are moving the files
+if os.path.isdir(sys.argv[-1]):
+    destination = sys.argv[-1]
 else:
     print(f'ERROR: {sources[-1]} is not a directory')
     sys.exit(1)
 
-# execute command
-command = ["/bin/mv"] + [flag for flag in flags] + [source for source in sources] + [destination]
+# commands
+mv_command = ["/bin/mv"] + [flag for flag in flags] + [source for source in sources] + [destination]
+rsync_command = ["/bin/rsync", "--progress", "--remove-source-files"] + [flag for flag in flags] + [source for source in rsync_sources] + [destination]
 
-# try:
-#     resultado = subprocess.run(command, capture_output=True, text=True, check=True)
-# except subprocess.CalledProcessError as e:
-#     print("ERROR:", e)
-#     print("STDERR:", e.stderr)
-# except Exception as e:
-#     print("ERROR:", e)
+# execute commands
+if len(sources) != 0:
+    subprocess.run(mv_command, capture_output=True, text=True, check=True)
+
+if len(rsync_sources) != 0:
+    subprocess.run(rsync_command, text=True, check=True)
