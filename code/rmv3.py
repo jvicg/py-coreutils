58b6aa20945c533db0ca16ad62a7ff0026b210a6
nrk19 <j@nrk.com>
2023-12-07 15:07:24 +0100

    minor changes

--- code/rmv3.py
+++ code/rmv3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+
 # py-coreutils/rmv3.py
-# script to replace rm - it will move files to a trash dir
+# script to remove files or move files to TRASH
 
 import os
 import sys
@@ -9,24 +10,25 @@ import argparse
 import subprocess
 from datetime import datetime
 
-HOME_DIR = os.path.expanduser('~')
-TRASH_DIR = os.path.join(HOME_DIR, ".trash")
+HOME_DIR = os.path.expanduser('~')            # home directory
+TRASH = ".trash"                              # name of the trash directory (modify if u wish)
+TRASH_DIR = os.path.join(HOME_DIR, TRASH)     # trash path
 
-# handle time to name trash files
-now = datetime.now()
-date = now.strftime("-%d-%b-%Y-%T") + ".trash"
+# handle file naming
+now = datetime.now()                          # start clock
+DATE = now.strftime("-%d-%b-%Y-%T")           # date as string
+TRASH_FORMAT = DATE + TRASH                   # add TRASH extension - format: {filename}-{date}.{TRASH}
 
-# create trash dir if didnt exists
-if not os.path.isdir(TRASH_DIR):
-    os.makedirs(TRASH_DIR)
+# create trash dir if dont exists
+if not os.path.isdir(TRASH_DIR): os.makedirs(TRASH_DIR)
 
-# handle arguments
+# program definition
 parser = argparse.ArgumentParser(
     prog="rmv3",
     description='rmv3 - simple script to remove or move files to TRASH',
     epilog = "py-coreutils/rmv3 - script part of the repo: github.com/nrk19/py-coreutils")
 
-# flags
+# arguments - flags
 parser.add_argument(
     '-v', '--verbose',
     action='store_true',
@@ -47,14 +49,14 @@ parser.add_argument(
     action='store_true',
     help='delete the files instead of sending them to TRASH')
 
-# files
+# argument - files
 parser.add_argument(
     'files',
     nargs="+",
     metavar="file",
-    help='files/dirs to be processed')
+    help='files/dirs to be removed')
 
-args = parser.parse_args()        # store arguments
+args = parser.parse_args()        # arg object
 verbose = args.verbose            # verbose
 recursive = args.recursive        # true if recursive flag was selected
 force = args.force                # force flag
@@ -62,14 +64,13 @@ delete = args.delete              # delete flag
 
 # execution of the program
 for f in args.files:
-
     # files
     if os.path.isfile(f):
         if delete:
             funcs.delete(f, verbose)
         else:
             f_basename = funcs.get_basename(f)
-            destination = os.path.join(TRASH_DIR, f_basename + date)
+            destination = os.path.join(TRASH_DIR, f_basename + TRASH_FORMAT)
             funcs.to_trash(f, destination, verbose)
     # dirs
     elif os.path.isdir(f):
@@ -78,7 +79,7 @@ for f in args.files:
             sys.exit(1)
         elif not delete:
             dir_name = funcs.get_basename(f)
-            destination = os.path.join(TRASH_DIR, dir_name + date)
+            destination = os.path.join(TRASH_DIR, dir_name + TRASH_FORMAT)
             funcs.to_trash(dir_name, destination, verbose)
         else:
             funcs.delete(f, verbose)
