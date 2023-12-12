fd3798db74af24ae0168c721e64a15c27eae61c6
nrk19 <j@nrk.com>
2023-12-12 19:47:43 +0100

    encapsuling code into main function

--- code/mvv3.py
+++ code/mvv3.py
@@ -21,60 +21,64 @@ destination = sys.argv[-1]       # destination always will be last argument pass
 sources = []                     # all the files or dirs that will be moved
 rsync_sources = []               # same but with progress bar
 
-# no arguments given case
-if len(sys.argv) < 3:
-    print("ERROR: no arguments where given")
-    funcs.print_mvv3_usage()
-    sys.exit(1)
-
-# process move/copy flag - must be the first argument (sys.argv[1])
-COPY_FLAGS = ["--copy", "-c"]    # valid copy flags
-MOVE_FLAGS = ["--move", "-m"]    # valid move flags
-selected_mode = "mv"             # the mode selected by the user (will be set as "mv" by default)
-
-if sys.argv[1] in COPY_FLAGS:
-    mv = ["/bin/cp"]
-    rsync = ["/bin/rsync", "-av", "--progress"]
-    selected_mode = "cp"
-elif sys.argv[1] in MOVE_FLAGS:
-    mv = ["/bin/mv"]
-    rsync = ["/bin/rsync", "-av", "--progress", "--remove-source-files"]
-else:
-    print("ERROR: an option must be specified")
-    funcs.print_mvv3_usage()
-    sys.exit(1)
-
-# handle the rest of the arguments
-for arg in sys.argv[2:-1]:
-    # strings with the pattern '-*' will be considered as flags
-    if arg.startswith("-"):
-        flags.append(arg)
-    # check is args are valid files/dirs
-    elif os.path.isfile(arg) or os.path.isdir(arg):
-        # check file size
-        if funcs.get_size(arg) > SIZE_FOR_PROGRESS_BAR:
-            rsync_sources.append(arg)
-        else:
-            sources.append(arg)
-    # error handling
-    else:
-        print(f'ERROR: {arg} is not a valid file or directory')
+def main():
+    # no arguments given case
+    if len(sys.argv) < 3:
+        print("ERROR: no arguments where given")
         funcs.print_mvv3_usage()
         sys.exit(1)
 
-# if last argument is not a directory, the program will assume that user wants to rename the file
-# so we just use regular /bin/mv (no matter whats the file size)
-if not os.path.isdir(destination) and selected_mode == "mv":
-    rsync = mv
+    # process move/copy flag - must be the first argument (sys.argv[1])
+    COPY_FLAGS = ["--copy", "-c"]    # valid copy flags
+    MOVE_FLAGS = ["--move", "-m"]    # valid move flags
+    selected_mode = "mv"             # the mode selected by the user (will be set as "mv" by default)
 
-# commands composition
-no_progress_bar_command = mv + [flag for flag in flags] + [source for source in sources] + [destination]
-progress_bar_command = rsync + [flag for flag in flags] + [source for source in rsync_sources] + [destination]
+    if sys.argv[1] in COPY_FLAGS:
+        mv = ["/bin/cp"]
+        rsync = ["/bin/rsync", "-av", "--progress"]
+        selected_mode = "cp"
+    elif sys.argv[1] in MOVE_FLAGS:
+        mv = ["/bin/mv"]
+        rsync = ["/bin/rsync", "-av", "--progress", "--remove-source-files"]
+    else:
+        print("ERROR: an option must be specified")
+        funcs.print_mvv3_usage()
+        sys.exit(1)
 
-# commands execution
-if len(sources) != 0:
-    funcs.run(no_progress_bar_command)
-if len(rsync_sources) != 0:
-    funcs.run(progress_bar_command)
+    # handle the rest of the arguments
+    for arg in sys.argv[2:-1]:
+        # strings with the pattern '-*' will be considered as flags
+        if arg.startswith("-"):
+            flags.append(arg)
+        # check is args are valid files/dirs
+        elif os.path.isfile(arg) or os.path.isdir(arg):
+            # check file size
+            if funcs.get_size(arg) > SIZE_FOR_PROGRESS_BAR:
+                rsync_sources.append(arg)
+            else:
+                sources.append(arg)
+        # error handling
+        else:
+            print(f'ERROR: {arg} is not a valid file or directory')
+            funcs.print_mvv3_usage()
+            sys.exit(1)
 
-sys.exit(0)
+    # if last argument is not a directory, the program will assume that user wants to rename the file
+    # so we just use regular /bin/mv (no matter whats the file size)
+    if not os.path.isdir(destination) and selected_mode == "mv":
+        rsync = mv
+
+    # commands composition
+    no_progress_bar_command = mv + [flag for flag in flags] + [source for source in sources] + [destination]
+    progress_bar_command = rsync + [flag for flag in flags] + [source for source in rsync_sources] + [destination]
+
+    # commands execution
+    if len(sources) != 0:
+        funcs.run(no_progress_bar_command)
+    if len(rsync_sources) != 0:
+        funcs.run(progress_bar_command)
+
+    sys.exit(0)
+
+if __name__=='__main__':
+    main()
